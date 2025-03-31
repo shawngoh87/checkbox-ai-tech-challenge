ddb65c07e6e4b0be834fc85f2f535a48148f3552
Shawn Goh <shawngoh87@gmail.com>
2025-03-31 17:57:21 +0800

    chore: remove unused imports

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,8 @@
-import { Kysely } from 'kysely';
-import { PostgresDialect } from 'kysely';
-import * as pg from 'pg';
-const { Pool } = pg.default;
-import express, { Router } from 'express';
+import express from 'express';
 import cors from 'cors';
 import { Container } from 'inversify';
 
 import logger from './utils/logger.js';
-import { Database } from './infra/database/types.js';
 import { TaskRepository } from './infra/repository/task/task.repository.js';
 import { ListTasksUseCase } from './application/use-case/task/list-tasks.js';
 import { CreateTaskUseCase } from './application/use-case/task/create-task.js';
@@ -15,7 +10,6 @@ import { UpdateTaskUseCase } from './application/use-case/task/update-task.js';
 import { CreateTaskController } from './application/controller/task/create-task.controller.js';
 import { ListTasksController } from './application/controller/task/list-tasks.controller.js';
 import { UpdateTaskController } from './application/controller/task/update-task.controller.js';
-import { getDatabaseServiceIdentifier } from './constants.js';
 import { registerDatabase } from './infra/database/database.provider.js';
 import { registerRoutes } from './application/route.provider.js';
 
